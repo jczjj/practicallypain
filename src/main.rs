733bbfd165113a03ebb3e41431d36faba6cbe006
jczjj <jasperczj1905@gmail.com>
2025-07-04 16:15:42 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{App, HttpServer, web};
 use sqlx::SqlitePool;
 use std::fs;
 use anyhow::Result;
-use actix_web::dev::Service;
+use actix_web::dev::{Service, ServiceRequest, ServiceResponse};
 
 mod handlers {
     pub mod auth;
